25d63ebeba3aab1fea8e8908a6f5f97d42cf3cfb
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-08-15 18:52:02 +0300

    feat: HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const sliceLimit = 10
+
+func Top10(s string) []string {
+	str := getByFrequency(strings.Fields(s))
+
+	return getSubSlice(str, sliceLimit)
+}
+
+func getByFrequency(s []string) []string {
+	frequency := make(map[string]int)
+	for _, c := range s {
+		frequency[c]++
+	}
+
+	sl := make([]string, 0, len(frequency))
+	for fr := range frequency {
+		sl = append(sl, fr)
+	}
+
+	sort.Slice(sl, func(i, j int) bool {
+		if frequency[sl[i]] == frequency[sl[j]] {
+			return lexicographicallySort(sl[i], sl[j])
+		}
+		return frequency[sl[i]] > frequency[sl[j]]
+	})
+
+	return sl
+}
+
+func lexicographicallySort(s1 string, s2 string) bool {
+	return sort.IsSorted(sort.StringSlice([]string{s1, s2}))
+}
+
+func getSubSlice(s []string, limit int) []string {
+	if len(s) >= limit {
+		return s[0:limit]
+	}
+
+	return s
 }
