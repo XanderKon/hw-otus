cad86b3f0a32a5a6a0d4c9460d3b8bf83846daf3
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-10-10 21:36:22 +0300

    feat: HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,15 +1,78 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+
+	ErrWriteFileError             = errors.New("there is some error with writing file")
+	ErrCannotCreateWriteFileError = errors.New("there is error with creating error file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileRead, err := os.OpenFile(fromPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	defer fileRead.Close()
+
+	// Получаем размер файла.
+	fi, _ := fileRead.Stat()
+	fileSize := fi.Size()
+
+	// Проверяем на корректное указание offset.
+	if offset != 0 && offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Если лимит не указан, пишем все.
+	if limit == 0 {
+		limit = fileSize
+	}
+
+	// Проверка на превышение лимита, если указан отступ.
+	if fileSize-offset < limit {
+		limit = fileSize - offset
+	}
+
+	return writer(fileRead, toPath, limit, offset)
+}
+
+func writer(src *os.File, toPath string, limit int64, offset int64) error {
+	file, err := os.Create(toPath)
+
+	if err != nil {
+		return ErrCannotCreateWriteFileError
+	}
+
+	// Инициализируем прогресс-бар.
+	bar := pb.Full.Start64(limit)
+
+	// Создаем ридер из слайса байт с указанными limit и offset
+	buf := make([]byte, limit)
+	src.ReadAt(buf, offset)
+	reader := bytes.NewReader(buf)
+
+	// Создаем прокси ридер, чтобы выводить прогресс-бар.
+	barReader := bar.NewProxyReader(reader)
+
+	if _, err := io.Copy(file, barReader); err != nil {
+		return ErrWriteFileError
+	}
+
+	// Завершаем прогресс-бар.
+	defer bar.Finish()
+	// Закрываем файл.
+	defer file.Close()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	// Создаем временный файл.
+	tempFile, _ := os.CreateTemp(".", "unittest-")
+	testTargetPath := tempFile.Name()
+
+	testSourcePath := "testdata/input.txt"
+	testSourcePathWithOffsetAndLimit := "testdata/out_offset100_limit1000.txt"
+
+	t.Run("case with original file", func(t *testing.T) {
+		err := Copy(testSourcePath, testTargetPath, 0, 0)
+		require.Nil(t, err)
+
+		origBytes, _ := os.ReadFile(testSourcePath)
+		targetBytes, err := os.ReadFile(testTargetPath)
+
+		require.Nil(t, err)
+		require.Equal(t, origBytes, targetBytes)
+	})
+
+	t.Run("case with offset and limit", func(t *testing.T) {
+		err := Copy(testSourcePath, testTargetPath, 100, 1000)
+		require.Nil(t, err)
+
+		origBytes, _ := os.ReadFile(testSourcePathWithOffsetAndLimit)
+		targetBytes, err := os.ReadFile(testTargetPath)
+
+		require.Nil(t, err)
+		require.Equal(t, origBytes, targetBytes)
+	})
+
+	t.Run("case with offset error", func(t *testing.T) {
+		err := Copy(testSourcePath, testTargetPath, 1000000, 0)
+		require.NotNil(t, err)
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+	})
+
+	// Удаляем временный файл.
+	defer os.Remove(testTargetPath)
 }
