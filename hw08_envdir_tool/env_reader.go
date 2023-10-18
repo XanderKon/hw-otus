b01e0faee876dc9428f1894f5345d969ed26edc4
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-10-18 22:24:12 +0300

    feat: HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+)
+
+var (
+	ErrReadingEnvironmentDir  = errors.New("cannot read target dir")
+	ErrUnknowFileInfo         = errors.New("cannot get file info")
+	ErrReadingEnvironmentFile = errors.New("cannot read environment file")
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +24,66 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envFiles, err := os.ReadDir(dir)
+
+	env := make(Environment)
+
+	if err != nil {
+		return nil, ErrReadingEnvironmentDir
+	}
+
+	for _, envFile := range envFiles {
+		fileInfo, err := envFile.Info()
+		if err != nil {
+			return nil, ErrUnknowFileInfo
+		}
+
+		envVarName := fileInfo.Name()
+
+		// skip reading env if file is empty.
+		if fileInfo.Size() == 0 {
+			env[envVarName] = EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			}
+			continue
+		}
+
+		// reading env.
+		val, err := readEnvFromFile(fileInfo, dir)
+		if err != nil {
+			return nil, err
+		}
+
+		env[envVarName] = EnvValue{
+			Value:      val,
+			NeedRemove: false,
+		}
+	}
+
+	return env, nil
+}
+
+func readEnvFromFile(envFile os.FileInfo, dir string) (string, error) {
+	f, err := os.Open(dir + "/" + envFile.Name())
+	if err != nil {
+		f.Close()
+		return "", ErrReadingEnvironmentFile
+	}
+	r := bufio.NewReader(f)
+	val, _, err := r.ReadLine()
+	if err != nil {
+		f.Close()
+		return "", ErrReadingEnvironmentFile
+	}
+
+	f.Close()
+	return parseEnvValue(val), nil
+}
+
+// cut spaces and tabs and replace terminal null.
+func parseEnvValue(value []byte) string {
+	parsed := string(value)
+	parsed = strings.ReplaceAll(parsed, "\x00", "\n")
+	return strings.TrimRight(parsed, ` 	`)
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,51 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := make(Environment)
+
+	env["FOO"] = EnvValue{
+		Value:      "TEST",
+		NeedRemove: false,
+	}
+	env["BAR"] = EnvValue{
+		Value:      "HELLO",
+		NeedRemove: false,
+	}
+
+	cmd := make([]string, 3)
+	cmd[0] = "/bin/bash"
+	cmd[1] = "./testdata/echo.sh"
+	cmd[2] = "arg1=test1 arg2=test2"
+
+	t.Run("case without error", func(t *testing.T) {
+		exitCode := RunCmd(cmd, env)
+
+		val, exist := os.LookupEnv("FOO")
+		require.Equal(t, val, "TEST")
+		require.Equal(t, exist, true)
+
+		val, exist = os.LookupEnv("BAR")
+		require.Equal(t, val, "HELLO")
+		require.Equal(t, exist, true)
+
+		require.Equal(t, exitCode, 0)
+	})
+
+	t.Run("case with error", func(t *testing.T) {
+		exitCode := RunCmd([]string{}, env)
+		require.Equal(t, exitCode, -1)
+
+		exitCode = RunCmd([]string{"/bin/bash", "test"}, env)
+		require.Equal(t, exitCode, 126)
+
+		exitCode = RunCmd([]string{"/bin/bash", "<1"}, env)
+		require.Equal(t, exitCode, 127)
+	})
 }
