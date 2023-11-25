c8c7f0bd46f48e6b044bfa16707580d91e946999
Кондратенко Александр <kondratenkoaa2@edu.mos.ru>
2023-11-25 23:08:40 +0300

    feat: Add Viper for building config

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
 type Config struct {
-	Logger LoggerConf
+	Logger LoggerConf `mapstructure:"logger"`
 	// TODO
 }
 
 type LoggerConf struct {
-	Level string
+	Level string `mapstructure:"level"`
+	Path  string `mapstructure:"path"`
 	// TODO
 }
 
-func NewConfig() Config {
-	return Config{}
-}
+func NewConfig() *Config {
+	v := viper.New()
+	v.SetConfigFile(configFile)
 
-// TODO
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("couldn't load config: %s", err)
+		os.Exit(1)
+	}
+	var config Config
+	if err := v.Unmarshal(&config); err != nil {
+		fmt.Printf("couldn't read config: %s", err)
+	}
+
+	return &config
+}
